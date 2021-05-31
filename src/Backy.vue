2c2ba4ca53efecc72b947e2da117335be901858b
unknown <rahzy24@gmail.com>
2021-05-31 22:26:49 +0100

    Contact Fuctionality Enacted

---
+++ src/Backy.vue
@@ -0,0 +1,69 @@
+<template>
+<div>
+  <!-- <Home/> -->
+  <!-- <Pricing/> -->
+  <!-- <About/> -->
+  <Contact/>
+
+</div>
+</template>
+
+<script>
+// import Home from './Home.vue'
+// import Pricing from './Pricing.vue'
+// import About from './About.vue'
+import Contact from './Contact.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // Home,
+    // Pricing,
+    // About,
+    Contact
+  }
+}
+</script>
+
+<style>
+
+.keep {
+    --dark-pink: #ba4270;
+    --link-water-white: #fbfcfe;
+    --san-juan-blue: #36536b;
+    --mirage-blue: #1b262f;
+    --charm-pink: #da6d97;
+    --light-san-juan-blue: #6c8294;
+}
+.showcase-text{
+    color:#36536b;
+}
+.dark-pink{
+   color:#ba4270;
+}
+
+.body-bg{
+  background-color: #EDF3F8;
+}
+
+.pts-font{
+  font-family: 'PT Serif', serif;
+}
+
+.public-sans{
+  font-family: 'Source Serif Pro', serif;
+}
+/* .public-sans{
+  font-family: 'Public Sans', sans-serif
+} */
+
+.generic-bg{
+    background: #1b262f;
+}
+.flexer{
+  display: flex;
+  justify-items: space-between;
+  justify-items: center;
+}
+
+</style>
